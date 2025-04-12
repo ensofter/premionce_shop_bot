ead14b36cad05bc1a105cb81f6fa8a5efdf2cfff
Romashov Aleksey <bogestvenniyiri@yandex.ru>
2025-04-13 00:04:40 +0300

    Add start command and welcome text

--- handlers/handlers.py
+++ handlers/handlers.py
@@ -1,5 +1,5 @@
 from aiogram import Router
-from aiogram.filters import CommandStart
+from aiogram.filters import CommandStart, Command
 from aiogram.types import Message
 from lexicon.lexicon_cmd import LEXICON_CMD
 
@@ -12,4 +12,11 @@ async def handle_cmd_start(message: Message):
     await message.answer_photo(
         photo=LEXICON_CMD['welcome_photo_id'],
         caption=LEXICON_CMD['/start']
+    )
+
+
+@router.message(Command(commands='help'))
+async def handle_cmd_help(message: Message):
+    await message.answer(
+        text=LEXICON_CMD['/help']
     )
