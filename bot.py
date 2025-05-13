1c850527ff1c0a6c90926ca96305e2d80b17dcf8
Romashov Aleksey <bogestvenniyiri@yandex.ru>
2025-05-13 16:12:23 +0300

    distrubuted handler to different files

--- bot.py
+++ bot.py
@@ -8,8 +8,11 @@ from aiogram.client.default import DefaultBotProperties
 
 from confing.config import Config
 from aiogram.enums import ParseMode
-from handlers import handlers
+from handlers import about_handlers
+from handlers import main_menu_handlers
+from handlers import referral_handlers
 from handlers import other_handlers
+from handlers import cmd_handlers
 
 conf = Config.load_config()
 logger = logging.getLogger()
@@ -22,7 +25,13 @@ async def main():
     bot = Bot(token=conf.tg_conf.bot_token, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
     dp = Dispatcher()
 
-    dp.include_routers(handlers.router, other_handlers.router)
+    dp.include_routers(
+        cmd_handlers.router,
+        main_menu_handlers.router,
+        referral_handlers.router,
+        about_handlers.router,
+        other_handlers.router
+    )
 
     await dp.start_polling(bot)
 
---
+++ handlers/cmd_handlers.py
@@ -0,0 +1,33 @@
+from copy import deepcopy
+
+from aiogram import Router
+from aiogram.filters import CommandStart, Command
+from aiogram.types import Message
+
+from database.database import user_db, user_dict_template
+from keyboards.main_menu_kb import create_main_menu_reply_kb
+from lexicon.lexicon_cmd import LEXICON_CMD
+
+router = Router()
+
+
+@router.message(CommandStart())
+async def handle_cmd_start(message: Message):
+    user_id = message.from_user.id
+    if user_id not in user_db:
+        user_db[user_id] = deepcopy(user_dict_template)
+
+    await message.answer_photo(
+        photo=LEXICON_CMD['welcome_photo_id'],
+        caption=LEXICON_CMD['/start'],
+        reply_markup=create_main_menu_reply_kb(
+            3, 'catalog', 'promotions', 'cart', 'orders', 'profile', 'referral', 'about'
+        )
+    )
+
+
+@router.message(Command(commands='help'))
+async def handle_cmd_help(message: Message):
+    await message.answer(
+        text=LEXICON_CMD['/help']
+    )
