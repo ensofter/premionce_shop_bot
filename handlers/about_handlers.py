b9b30f42f4ac8cf054e9d3893b94cb447212ec5a
Romashov Aleksey <bogestvenniyiri@yandex.ru>
2025-08-03 19:00:39 +0300

    many code

--- handlers/about_handlers.py
+++ handlers/about_handlers.py
@@ -14,8 +14,10 @@ router = Router()
 logger = logging.getLogger(__name__)
 
 
+# Хэндлер обрабатывающий нажатие кнопки получения списка FAQ
 @router.callback_query(F.data.in_(['faq', 'back_to_faq']))
-async def handle_clbck_button_faq_pressed(callback: CallbackQuery):
+async def handle_button_faq_pressed(callback: CallbackQuery):
+    user_id = callback.from_user.id
     questions = [i for i in LEXICON_FAQ_QUESTIONS.keys()]
     inline_kb = create_inline_kb(
         1,
@@ -23,14 +25,17 @@ async def handle_clbck_button_faq_pressed(callback: CallbackQuery):
         *questions
     )
     inline_kb.row(InlineKeyboardButton(text=LEXICON_COMMON['back'], callback_data='back_to_about'))
+    logger.info(f'Отдаем пользователю {user_id} список часто задаваемых вопросов FAQ')
     await callback.message.edit_text(
         text='👨‍🏫 Выберите вопрос',
         reply_markup=inline_kb.as_markup()
     )
 
 
+# Хэндлер обрабатывающий нажатие кнопки вопроса в списке вопросов FAQ
 @router.callback_query(F.data.in_([i for i in LEXICON_FAQ_QUESTIONS.keys()]))
-async def handle_clbck_button_question_pressed(callback: CallbackQuery):
+async def handle_button_question_pressed(callback: CallbackQuery):
+    user_id = callback.from_user.id
     questions_keys = [i for i in LEXICON_FAQ_QUESTIONS.keys()]
     current_item = callback.data
     current_index = questions_keys.index(current_item)
@@ -43,14 +48,17 @@ async def handle_clbck_button_question_pressed(callback: CallbackQuery):
         f'forward:{current_item}' if show_next else None
     )
     reply_kb.row(InlineKeyboardButton(text=LEXICON_COMMON['back'], callback_data='back_to_faq'))
+    logger.info(f'Пользователь {user_id} выбрал вопрос {LEXICON_FAQ_QUESTIONS[callback.data]} FAQ для прочтения')
     await callback.message.edit_text(
         text=LEXICON_FAQ_QUESTIONS[callback.data],
         reply_markup=reply_kb.as_markup()
     )
 
 
+# Хэндлер обрабатывающий нажатие кнопки пагинации НАЗАД в списке вопросов FAQ
 @router.callback_query(F.data.split(':')[0] == 'backward')
-async def handle_clbck_button_backward_pressed(callback: CallbackQuery):
+async def handle_button_backward_pressed(callback: CallbackQuery):
+    user_id = callback.from_user.id
     questions_keys = [i for i in LEXICON_FAQ_QUESTIONS.keys()]
     current_item = callback.data.split(':')[1]
     current_index = questions_keys.index(current_item)
@@ -63,7 +71,7 @@ async def handle_clbck_button_backward_pressed(callback: CallbackQuery):
             f'forward:{prev_key}'
         )
         reply_kb.row(InlineKeyboardButton(text=LEXICON_COMMON['back'], callback_data='back_to_faq'))
-
+        logger.info(f'Пользователь {user_id} нажал кнопку получения предыдущего вопроса FAQ')
         await callback.message.edit_text(
             text=LEXICON_FAQ_QUESTIONS[prev_key],
             reply_markup=reply_kb.as_markup()
@@ -71,8 +79,10 @@ async def handle_clbck_button_backward_pressed(callback: CallbackQuery):
     await callback.answer()
 
 
+# Хэндлер обрабатывающий нажатие кнопки пагинации ВПЕРЕД в списке вопросов FAQ
 @router.callback_query(F.data.split(':')[0] == 'forward')
-async def handle_clbck_button_forward_pressed(callback: CallbackQuery):
+async def handle_button_forward_pressed(callback: CallbackQuery):
+    user_id = callback.from_user.id
     questions_keys = [i for i in LEXICON_FAQ_QUESTIONS.keys()]
     current_item = callback.data.split(':')[1]
     current_index = questions_keys.index(current_item)
@@ -86,7 +96,7 @@ async def handle_clbck_button_forward_pressed(callback: CallbackQuery):
             f'forward:{next_key}' if show_next else None
         )
         reply_kb.row(InlineKeyboardButton(text=LEXICON_COMMON['back'], callback_data='back_to_faq'))
-
+        logger.info(f'Пользователь {user_id} нажал кнопку получения следующего вопроса FAQ')
         await callback.message.edit_text(
             text=LEXICON_FAQ_QUESTIONS[next_key],
             reply_markup=reply_kb.as_markup()
@@ -94,14 +104,17 @@ async def handle_clbck_button_forward_pressed(callback: CallbackQuery):
     await callback.answer()
 
 
+# Хэндлер обрабатывающий нажатие кнопки ОФЕРТЫ
 @router.callback_query(F.data == 'offer')
-async def hancle_clbck_button_offer_pressed(callback: CallbackQuery):
+async def handle_button_offer_pressed(callback: CallbackQuery):
+    user_id = callback.from_user.id
     text = LEXICON_OFFER['offer']
     inline_kb = create_inline_kb(
         1,
         LEXICON_ABOUT,
         'back_to_about'
     )
+    logger.info(f'Пользователь {user_id} нажал кнопку получения Оферты')
     await callback.message.edit_text(
         text=text,
         reply_markup=inline_kb.as_markup()
